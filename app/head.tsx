c6f42917a9d455a168a6f998f1d161814d32f59e
BRICKENGLAS <jcrespom@iesalcorisa.es>
2026-02-18 19:44:21 +0100

    Update meta description and favicon link

--- app/head.tsx
+++ app/head.tsx
@@ -2,9 +2,8 @@ export default function Head() {
   return (
     <>
       <title>1 Risa Por Mes - 1RXM</title>
-      <meta name="description" content="Red social de chistes" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="Red social de chistes, comparte tu risa con el mundo" />
+      <link rel="icon" href="/1rxm-favicon.ico" />
     </>
   )
 }
-
